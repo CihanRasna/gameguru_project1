9bf681d230baff079e3e4d2e3c4810ebc90de380
Cihan Rasna <Cihanrasna@gmail.com>
2025-06-28 13:36:31 +0300

    feat : input field for update grid size

--- Assets/_Project/Scripts/StartGrid.cs
+++ Assets/_Project/Scripts/StartGrid.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI; // InputField, Button için
 using Zenject;
 
 namespace _Project.Scripts
@@ -6,6 +8,8 @@ namespace _Project.Scripts
     public class StartGrid : MonoBehaviour
     {
         [Inject] private GridManager _gridManager;
+
+        [Header("Grid Settings")]
         [SerializeField] private GridCell cellPrefab;
         [SerializeField] private Transform gridParent;
         [SerializeField] private int gridSize = 6;
@@ -13,12 +17,27 @@ namespace _Project.Scripts
         [SerializeField] private float cellSingleScaleUpTime = 0.1f;
         [SerializeField] private float initializeDuration = 5f;
 
+        [Header("UI")]
+        [SerializeField] private TMP_InputField gridSizeInputField;
+        [SerializeField] private Button resizeButton;
+
         private void Start()
         {
-            _gridManager.Initialize(gridSize, cellSizeOffset, initializeDuration, cellSingleScaleUpTime, cellPrefab,
-                gridParent);
+            _gridManager.Initialize(gridSize, cellSizeOffset, initializeDuration, cellSingleScaleUpTime, cellPrefab, gridParent);
+
+            if (resizeButton != null && gridSizeInputField != null)
+            {
+                resizeButton.onClick.AddListener(OnResizeButtonClicked);
+            }
         }
 
+        private void OnResizeButtonClicked()
+        {
+            if (int.TryParse(gridSizeInputField.text, out var newSize))
+            {
+                _gridManager.ResizeGrid(newSize, cellSizeOffset, initializeDuration, cellSingleScaleUpTime, cellPrefab, gridParent);
+            }
+        }
 
         [ContextMenu("Resize Grid")]
         public void ResizeGridFromEditor(int newGridSize)
@@ -29,8 +48,7 @@ namespace _Project.Scripts
                 return;
             }
 
-            _gridManager.ResizeGrid(newGridSize, cellSizeOffset, initializeDuration, cellSingleScaleUpTime, cellPrefab,
-                gridParent);
+            _gridManager.ResizeGrid(newGridSize, cellSizeOffset, initializeDuration, cellSingleScaleUpTime, cellPrefab, gridParent);
         }
     }
 }
